c700747e21355dab97046e7006c4460189bcb8a6
Murugesan, Sarathi <Parthasarathi.Murugesan@sabre.com>
2021-12-08 18:44:19 -0600

    Initial version of Custom AVPlayer app

---
+++ AVPlayerTest/CustomAVPlayerView.swift
@@ -0,0 +1,132 @@
+//
+//  CustomAVPlayerView.swift
+//  AVPlayerTest
+//
+//  Created by Parth Sarathi on 12/8/21.
+//
+
+import Foundation
+import AVFoundation
+import UIKit
+
+protocol PlayerDelegate: AnyObject {
+    func playStarted()
+    func playFinished()
+    func halfSecondPeriodicObserver(time: CMTime)
+}
+
+let timeScale = CMTimeScale(NSEC_PER_SEC)
+
+class CustomAVPlayerView: UIView {
+    
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+    
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+        
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    private var playerItemContext = 0
+
+    private var playerItem: AVPlayerItem?
+    
+    private var timeObserverToken: Any?
+
+    weak var delegate: PlayerDelegate?
+
+    private var seekTo: Int64?
+    
+    //MARK: AVPlayer Methods
+    func play(with url: URL, seekToSeconds: Int64? = nil) {
+        setUpPlayerItem(with: url)
+        
+        //seek to seconds if available
+        self.seekTo = seekToSeconds
+    }
+    
+    private func setUpPlayerItem(with url: URL) {
+        let asset = AVAsset(url: url)
+        playerItem = AVPlayerItem(asset: asset, automaticallyLoadedAssetKeys: ["playable", "hasProtectedContent"])
+
+        setupObserver(for: playerItem)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.setupPlayer(playerItem: self?.playerItem)
+        }
+    }
+
+    private func setupPlayer(playerItem: AVPlayerItem?) {
+        player = AVPlayer(playerItem: playerItem!)
+        let time = CMTime(seconds: 0.5, preferredTimescale: timeScale)
+        
+        timeObserverToken = player?.addPeriodicTimeObserver(forInterval: time, queue: .main) { [weak self] time in
+            self?.delegate?.halfSecondPeriodicObserver(time: time)
+        }
+    }
+    
+    //MARK: Observer Methods
+    private func setupObserver(for playerItem: AVPlayerItem?) {
+        playerItem?.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.old, .new], context: &playerItemContext)
+        playerItem?.addObserver(self, forKeyPath:  #keyPath(AVPlayerItem.timebase), options: [.old, .new], context: &playerItemContext)
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinish(_:)), name: .AVPlayerItemDidPlayToEndTime, object: playerItem)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard context == &playerItemContext else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+        
+        if keyPath == #keyPath(AVPlayerItem.status) {
+            let status: AVPlayerItem.Status
+            if let statusNumber = change?[.newKey] as? NSNumber {
+                status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+            } else {
+                status = .unknown
+            }
+            switch status {
+            case .readyToPlay:
+                player?.play()
+                
+                //Seek to last to check finished play
+                if let time = seekTo {
+                    player?.seek(to: CMTimeMake(value: time, timescale: 1))
+                }
+            case .failed, .unknown:
+                print(status)
+            default:
+                print("default")
+            }
+        } else if keyPath == #keyPath(AVPlayerItem.timebase) {
+            if let rate = player?.rate, rate > 0 {
+                delegate?.playStarted()
+            }
+        }
+    }
+    
+    @objc func playerDidFinish(_ notification: Notification) {
+        delegate?.playFinished()
+     }
+    
+    //MARK: Deinit
+    deinit {
+        playerItem?.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.status))
+        playerItem?.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.timebase))
+        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: playerItem)
+        
+        if let timeObserverToken = timeObserverToken {
+            player?.removeTimeObserver(timeObserverToken)
+            self.timeObserverToken = nil
+        }
+    }
+}
---
+++ AVPlayerTest/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  AVPlayerTest
+//
+//  Created by Parth Sarathi on 12/8/21.
+//
+
+import UIKit
+import CoreMedia
+
+let videoUrl = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8"
+
+class ViewController: UIViewController, PlayerDelegate {
+
+    @IBOutlet weak var playerView: CustomAVPlayerView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupCustomAVPlayerView()
+    }
+    
+    func setupCustomAVPlayerView() {
+        guard let url = URL(string: videoUrl) else {
+            print("Invalid URL")
+            return
+        }
+        
+        playerView.play(with: url)
+        playerView.delegate = self
+    }
+    
+    func playStarted() {
+        print("Play Started")
+        activityIndicator.stopAnimating()
+    }
+    
+    func playFinished() {
+        print("Play Finished")
+    }
+    
+    func halfSecondPeriodicObserver(time: CMTime) {
+        print("Current PlayHead", time.seconds)
+    }
+    
+}
+
